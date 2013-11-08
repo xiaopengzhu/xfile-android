9c22d1043bfdb0f950e92f16a69fecf8a2ff5874
xiaopengzhu <xp_zhu@qq.com>
2013-11-08 17:31:27 +0800

    2

--- src/cn/com/xfile/ListActivity.java
+++ src/cn/com/xfile/ListActivity.java
@@ -1,6 +1,13 @@
 package cn.com.xfile;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
@@ -13,21 +20,51 @@ import org.json.JSONException;
 import org.json.JSONTokener;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.SimpleAdapter.ViewBinder;
 
 public class ListActivity extends Activity{
     private String url = "http://www.xpcms.net/mobile.php/api/getTypes";
+    private ListView listview;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
-        getData();
         
+        SimpleAdapter simpleadapter = new SimpleAdapter(this, getData(),
+                R.layout.activity_list_item,
+                new String[]{"id", "name", "icon"}, 
+                new int[]{R.id.item_id, R.id.item_name, R.id.item_icon});
+        simpleadapter.setViewBinder(new ViewBinder() {
+            
+            @Override
+            public boolean setViewValue(View view, Object data,
+                    String textRepresentation) {
+                // TODO Auto-generated method stub
+                if (view instanceof ImageView && data instanceof Bitmap) {
+                    ImageView iv = (ImageView) view;
+                    iv.setImageBitmap((Bitmap)data);
+                    return true;
+                } else 
+                return false;
+            }
+        });
+        
+        listview = (ListView)findViewById(R.id.listview);
+        listview.setAdapter(simpleadapter);
     }
     
-    private String getData() {
+    private List<HashMap<String, Object>> getData() {
+        ArrayList<HashMap<String, Object>> list = new ArrayList<HashMap<String,Object>>();
+        HashMap<String, Object> map = null;
+        
         HttpGet get = new HttpGet(url);
         HttpClient client = new DefaultHttpClient();
         try {
@@ -35,7 +72,11 @@ public class ListActivity extends Activity{
             String str = EntityUtils.toString(response.getEntity());
             JSONArray line = new JSONArray(new JSONTokener(str));
             for (int i =0; i<line.length();i++) {
-                Log.v("debug", line.getJSONObject(i).getString("icon"));
+                map = new HashMap<String, Object>();
+                map.put("id", line.getJSONObject(i).getString("id"));
+                map.put("name", line.getJSONObject(i).getString("name"));
+                map.put("icon", getBitMap("http://www.xpcms.net/public/upload/type/"+line.getJSONObject(i).getString("icon")));
+                list.add(map);
             }
         } catch (ClientProtocolException e) {
             // TODO Auto-generated catch block
@@ -47,7 +88,24 @@ public class ListActivity extends Activity{
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        return null;
+        return list;
         
+    }
+    
+    private Bitmap getBitMap(String str) {
+        Bitmap img = null;
+        try {
+            URL url = new URL(str);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            InputStream is = conn.getInputStream();
+            img = BitmapFactory.decodeStream(is); 
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return img;
     }
 }
